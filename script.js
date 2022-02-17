bcd7f124f417a32827e909889b82cc828bdce89c
Entriopic <bruno.adlesic@gmail.com>
2022-02-17 20:39:45 +0100

    new functions and bugfixes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let display = document.querySelector('#display');
 let clear = document.querySelector('#clear');
 let deleteBtn = document.querySelector('#delete');
-let buttons = document.querySelectorAll('.nums-ops');
+let buttons = document.querySelectorAll('.nums');
 let equals = document.querySelector('#equals');
 let operators = document.querySelectorAll('.operators');
 
 
-// basic functions
+
 function add(x, y) {
    return x + y;
 }
@@ -24,62 +24,66 @@ function divide(x, y) {
 }
 
 
-// operate function
-let operate = function(x, y, operator) {
-     switch(operator) {
-       case "+":
-          return add(x, y);
-       case "-":
-          return subtract(x, y);
-       case "*":
-          return multiply(x, y);
-       case "/":
-          return divide(x, y);
-       default: 
-          console.log("Yikes");         
-    } 
+let operate = function(x, y, oper) {
+
+   switch(oper) {
+      case "+":
+         return add(x, y);
+      case "-":
+         return subtract(x, y);
+      case "*":
+         return multiply(x, y);
+      case "/":
+         return divide(x, y);
+      default: 
+         console.log("Yikes");         
+   } 
 };
 
 
-//variables for storing nums
+display.innerText = 0;
 let firstNum = null;
 let secondNum = null;
 let result = null;
-let hasDecimal = false;
+let storedOperator = null;
+let secondOperator = false;
+let clearBeforeInput = false;
 
 
 //each click on button displays a number on the display
 buttons.forEach(button => {
    button.addEventListener('click', function(e) {
-     display.innerText += e.currentTarget.textContent;
+    enableOperators()
+    if(clearBeforeInput) {
+          display.innerText = "";
+          clearBeforeInput = false;
+      }
+      if(display.innerText == 0) display.innerText = "";
+      if(e.target.textContent == '.' && display.innerText.includes('.')) return;
+
+   display.innerText += e.currentTarget.textContent;
    })
 })
 
 
-//update firstNum variable when operator is clicked
 operators.forEach(operator => {
    operator.addEventListener('click', function(e) {
-     if(firstNum){
-       secondNum = display.innerText;
-       operateResult();
-       firstNum = `${result}${e.target.textContent}`;
-       secondNum = null;
-       display.innerText = "";
-     } else {
-       firstNum = display.innerText;
-       display.innerText = "";
-     }
+    disableOperators()
+   if(firstNum){
+      secondNum = display.innerText;
+      firstNum = operateResult(); 
+      storedOperator=`${e.target.textContent}`;
+      secondNum = null;
+      display.innerText = firstNum;
+      clearBeforeInput = true;
+   } else {
+      firstNum = display.innerText;
+      display.innerText = "";
+      storedOperator = e.target.textContent;
+   }
    })
 });
 
-
-//clear button, restoring variables to 0
-function clearAll() {
-   display.innerText = "";
-   firstNum = null;
-   secondNum = null;
-   result = null;
-}
 
 clear.addEventListener('click', function() {
    clearAll();
@@ -89,38 +93,55 @@ deleteBtn.addEventListener('click', function() {
    display.innerText = display.innerText.toString().slice(0, -1);
 })
 
-//when equals sign gets pressed it stores the value of display to secondNum variable
-equals.addEventListener('click', function() {
-   if(firstNum || secondNum === "Infinity") {
-      clearAll();
-   } else {
+
+equals.addEventListener('click', function(e) {
+   if (display.innerText == "") return;
+   if(e.target.textContent == '=' && (display.innerText.typeof === 'number')) return;
    secondNum = display.innerText;
-   operateResult();
-   display.innerText = result.toFixed(2);
+  console.log("operator->"+storedOperator)
+   result = operateResult();
+   if(result == "Infinity") {
+      setTimeout(() => {clearAll();}, 1000);
+      console.error("How do you split your chocolate for 0 people?");
+   } 
+   if(result % 1 != 0) {
+      display.innerText = result.toFixed(2);
+   }  else {
+      display.innerText = result;
+   }
    firstNum = null;
    secondNum = null;
-}
 });
 
-/*    if(firstNum || secondNum === "Infinity") {
-      clearAll();
-   } */
 
-//function for calculation
-function operateResult() {
-   if(firstNum.includes("+")) {
-      result = operate(parseFloat(firstNum), parseFloat(secondNum), "+");
-   } else if(firstNum.includes("-")) {
-      result = operate(parseFloat(firstNum), parseFloat(secondNum), "-");
-   } else if(firstNum.includes("*")) {
-      result = operate(parseFloat(firstNum), parseFloat(secondNum), "*");
-   } else if(firstNum.includes("/")) {
-      result = operate(parseFloat(firstNum), parseFloat(secondNum), "/");
-   }
+function enableOperators() {
+    operators.forEach(operator => {
+        operator.children[0].disabled=false;
+    })
 }
 
 
+function disableOperators() {
+   document.body.style.backgroundColor = "linear-gradient(top, #0e0e0e00  10%, hsl(120, 0%, 60%) 100%);";
+    operators.forEach(operator => {
+        operator.children[0].disabled=true;
+    })
 
+}
+
+
+function operateResult() {
+ result = operate(parseFloat(firstNum), parseFloat(secondNum), storedOperator);
+  return result
+}
+
+
+function clearAll() {
+   display.innerText = 0;
+   firstNum = null;
+   secondNum = null;
+   result = null;
+}
 
 
 
